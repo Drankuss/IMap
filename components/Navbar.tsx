a2851bb4e47c0fac4fb454d73f2e8c44a6de415c
Drankuss <kacperix727@wp.pl>
2023-12-16 17:41:11 +0100

    Authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,47 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { NavLinks } from '../constants'
-import AuthProviders from './AuthProviders';
+import Image from "next/image";
+import Link from "next/link";
+import { NavLinks } from "../constants";
+import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "../lib/session";
 
-const Navbar = () => {
-    const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
+
   return (
     <nav className="flexBetween navbar">
-        <div className="flex-1 flexStart gap-10">
-            <Link href="/">
-                <Image 
-                    src="/logo.svg" 
-                    width={160} 
-                    height={70} 
-                    alt="IMap" 
-                />
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={160} height={70} alt="IMap" />
+        </Link>
+        <ul className="xl:flex hidden tex-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
             </Link>
-            <ul className='xl:flex hidden tex-small gap-7'>
-                {NavLinks.map((link) => (
-                    <Link href={link.href} key={link.key}>
-                        {link.text}
-                    </Link>
-                ))}
-            </ul>
-        </div>
-        <div className='flexCenter gap-4'>
-            {session ? (
-                <>
-                    UserPhoto
-
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
-                </>
-            ) : (
-                <AuthProviders />
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session?.user ? (
+          <>
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
             )}
-        </div>
-    </nav>
-  )
-}
 
-export default Navbar
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,24 @@
-import Footer from '../../components/Footer';
-import Navbar from '../../components/Navbar';
-import './globals.css';
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import "./globals.css";
 
 export const metadata = {
-  title: 'IMap',
-  description: 'Interactive maps like never before',
-}
+  title: "IMap",
+  description: "Interactive maps like never before",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
